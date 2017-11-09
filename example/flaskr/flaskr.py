a0a026e82684283db0569cffde8877521e754b9a
tommycai123 <caixiao6666@126.com>
2017-11-09 09:52:13 +0800

    first commit

---
+++ example/flaskr/flaskr.py
@@ -0,0 +1,30 @@
+# all the imports
+import os
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+app = Flask(__name__)
+DATABASE = '/path/to/database.db'
+
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+    DEBUG=True,
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+def connect_db():
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+def init_db():
+    with app.app_context():
+        db = connect_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+if __name__ == '__main__':
+    init_db()
